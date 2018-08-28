d030d7dfb91199f1be571164a4648e3962171f4a
yrung <yrungen@163.com>
2018-08-29 00:44:21 +0800

    commit add Comment 1

--- src/com/define_observers/test/RunTest.java
+++ src/com/define_observers/test/RunTest.java
@@ -50,6 +50,7 @@ public class RunTest {
         developmentProgressData.setCurrentData(34, 45);
         //当项目经理出差了，不观察项目进度了就取消订阅了
         developmentProgressData.unRegisterObserver(projectManagerObserver);
+        //当前只有产品经理获取到数据
         developmentProgressData.setCurrentData(46, 90);
     }
 
